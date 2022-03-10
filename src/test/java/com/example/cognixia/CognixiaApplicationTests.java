fb59340b8b3bb4633d11b5b06786f36a268be064
Sai Kiran Challa <schalla@adobe.com>
2022-03-10 08:22:38 -0700

    Initial commit

---
+++ src/test/java/com/example/cognixia/CognixiaApplicationTests.java
@@ -0,0 +1,20 @@
+package com.example.cognixia;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CognixiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Test
+	void testSomething() {
+		assertTrue(true);
+	}
+
+}
